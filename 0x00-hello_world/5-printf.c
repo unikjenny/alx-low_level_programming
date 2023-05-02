0be4c84d9d2f3dcdf29ba67b3f887e79c76ba055
unikjenny <unikjenny663@gmail.com>
2023-05-02 02:14:10 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - Printing a line with printf function
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
