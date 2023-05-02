ff4bc593f39965cab094b3ea644eb451709681ce
unikjenny <unikjenny663@gmail.com>
2023-05-02 01:45:04 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
- * main - Puts function programming
+ * main - Puts funtion programming
  *
- * Return: Always 0 (Success) 
+ * Return: 0 (Success)
  */
-int main(void) 
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
+
